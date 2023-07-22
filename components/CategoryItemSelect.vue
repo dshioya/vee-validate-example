6ffaa93f54fb20cb0cbf351e1f0640e1e6dfd3cb
d.shioya <d.shioya@wiz-net.jp>
2023-07-22 21:38:09 +0900

    first commit

---
+++ components/CategoryItemSelect.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="category-item-select-ct">
+    <div class="category-field">
+      <select v-model="categoryValue">
+        <option v-for="option in categoryOptions"
+                :value="option.value">{{ option.label }}
+        </option>
+      </select>
+      <div v-if="categoryErrorMessage" class="field-error">{{ categoryErrorMessage }}</div>
+    </div>
+    <div class="item-field">
+      <select v-model="itemValue">
+        <option v-for="option in itemOptions"
+                :value="option.value">{{ option.label }}
+        </option>
+      </select>
+      <div v-if="itemErrorMessage" class="field-error">{{ itemErrorMessage }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {defineProps} from 'vue'
+import {useField} from 'vee-validate'
+import * as yup from 'yup'
+
+const props = defineProps({
+  categoryName: {
+    type: String,
+    required: true
+  },
+  itemName: {
+    type: String,
+    required: true
+  }
+})
+
+const rules = yup.string().required()
+
+const {
+  value: categoryValue,
+  errorMessage: categoryErrorMessage
+} = useField(props.categoryName, rules)
+
+const {
+  value: itemValue,
+  errorMessage: itemErrorMessage
+} = useField(props.itemName, rules)
+
+const categoryOptions = [
+  {
+    label: '',
+    value: ''
+  },
+  ...[1, 2, 3].map(i => ({
+    label: `カテゴリ${i}`,
+    value: `category${i}`
+  }))
+]
+
+const itemOptions = [
+  {
+    label: '',
+    value: ''
+  },
+  ...[1, 2, 3, 4, 5].map(i => ({
+    label: `項目${i}`,
+    value: `item${i}`
+  }))
+]
+</script>
+
+<style lang="scss">
+.category-item-select-ct {
+  display: flex;
+  gap: 8px;
+
+  select {
+    padding: 12px;
+  }
+
+  .field-error {
+    color: #dc2626;
+    font-size: 11px;
+  }
+}
+</style>
